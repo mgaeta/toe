0764a94f69456a91eb537a041a8e5764200e76fe
Marcos Gaeta <mgaeta89@gmail.com>
2014-07-18 20:56:59 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Toe::Application.config.secret_key_base = 'a2f58f0b5e0fbd7ae14ad45e7fecfb912f674dd12b2ce9f326f405089ae201736ea2e1032961c412a43bf9c63261bad2f39238619f299544d55ad152df70d33f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Toe::Application.config.session_store :cookie_store, key: '_toe_session'
